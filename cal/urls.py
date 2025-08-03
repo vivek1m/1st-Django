f1dda1fb974e47fca06b4050b7c8fae213237bff
Vivek Sharma <viveksharma12347620@gmail.com>
2025-08-03 13:44:38 +0530

    first commit

--- cal/urls.py
+++ cal/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('index/', views.index, name='index'),
+    path('submitquery/', views.submit_query, name='submit_query'),
     
 ]
--- cal/views.py
+++ cal/views.py
@@ -4,3 +4,7 @@ def home(request):
     return HttpResponse("Welcome to the Calculator App!")
 def index(request):
     return render(request, 'index.html')
+def submit_query(request):
+    q= request.GET.get('query')
+    return HttpResponse(q)
+   
